95012ba5582edb770e1f82ca19a54f4eb2967bb4
Jacky1100 <609615907@qq.com>
2017-08-09 11:04:40 +0800

    Jack update

--- src/main/java/com/paipianwang/activiti/task/listener/InputTeamDealLogTaskLisnter.java
+++ src/main/java/com/paipianwang/activiti/task/listener/InputTeamDealLogTaskLisnter.java
@@ -78,7 +78,7 @@ public class InputTeamDealLogTaskLisnter implements TaskListener {
 				dealLog.setLogType(1); // 出账
 				
 				// 获取 项目名称
-				PmsProjectFlow projectFlow = flowFacade.getProjectFlowByUniqueId(
+				PmsProjectFlow projectFlow = flowFacade.getProjectFlowByProcessInstanceId(
 						new ArrayList<String>(Arrays.asList("projectName")), processInstanceId);
 				dealLog.setProjectName(projectFlow.getProjectName());
 
