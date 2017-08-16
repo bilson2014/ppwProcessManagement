6e35a8f16a3d34344f79b43fbb0f31b5824e1721
lirui0 <rui000x@foxmail.com>
2017-08-16 14:28:50 +0800

    msg

--- src/main/java/com/paipianwang/activiti/resources/controller/MessageController.java
+++ src/main/java/com/paipianwang/activiti/resources/controller/MessageController.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 
-import org.activiti.engine.identity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,8 +11,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.alibaba.dubbo.common.utils.StringUtils;
 import com.paipianwang.activiti.domin.BaseMsg;
-import com.paipianwang.activiti.utils.UserUtil;
+import com.paipianwang.activiti.service.MessageService;
+import com.paipianwang.pat.common.constant.PmsConstant;
+import com.paipianwang.pat.common.entity.SessionInfo;
+import com.paipianwang.pat.common.util.ValidateUtil;
 import com.paipianwang.pat.workflow.entity.PmsProjectMessage;
 import com.paipianwang.pat.workflow.facade.PmsProjectMessageFacade;
 
@@ -23,6 +26,9 @@ public class MessageController {
 	
 	@Autowired
 	private PmsProjectMessageFacade pmsProjectMessageFacade;
+	@Autowired
+	private MessageService msMessageService;
+	
 	
 	/**
 	 * 添加留言topic
@@ -32,11 +38,11 @@ public class MessageController {
 	@RequestMapping(value="/addTopic" ,method = RequestMethod.POST,consumes="application/json")
 	public BaseMsg addTopicResource(@RequestBody final PmsProjectMessage message,final HttpServletRequest request) {
 		message.setParentId(null);
-		User loginUser=UserUtil.getUserFromSession(request.getSession());
-		if(loginUser==null){
-			//是否需要校验
+		final SessionInfo info = (SessionInfo) request.getSession().getAttribute(PmsConstant.SESSION_INFO);
+		message.setFromId(info.getActivitiUserId());
+		if(ValidateUtil.isValid(info.getActivitGroups())){
+			message.setFromGroup(StringUtils.join(info.getActivitGroups(), ","));
 		}
-		message.setFromId(loginUser.getId());
 		pmsProjectMessageFacade.insert(message);
 		BaseMsg result=new BaseMsg();
 		result.setCode(BaseMsg.NORMAL);
@@ -56,11 +62,11 @@ public class MessageController {
 			result.setErrorMsg("");
 			return result;
 		}
-		User loginUser=UserUtil.getUserFromSession(request.getSession());
-		if(loginUser==null){
-			//是否需要校验
+		final SessionInfo info = (SessionInfo) request.getSession().getAttribute(PmsConstant.SESSION_INFO);
+		pmsProjectMessage.setFromId(info.getActivitiUserId());
+		if(ValidateUtil.isValid(info.getActivitGroups())){
+			pmsProjectMessage.setFromGroup(StringUtils.join(info.getActivitGroups(), ","));
 		}
-		pmsProjectMessage.setFromId(loginUser.getId());
 		pmsProjectMessageFacade.insert(pmsProjectMessage);
 		
 		result.setCode(BaseMsg.NORMAL);
@@ -73,7 +79,7 @@ public class MessageController {
 	 */
 	@RequestMapping("/getDefaultMsg/{projectId}")
 	public List<PmsProjectMessage> getDefaultMessage(@PathVariable("projectId") String projectId){
-		return pmsProjectMessageFacade.getDefaultProjectMessage(projectId);
+		return msMessageService.getDefaultProjectMessage(projectId);
 	}
 	
 	/**
@@ -83,7 +89,7 @@ public class MessageController {
 	 */
 	@RequestMapping("/getProjectMsg/{projectId}")
 	public List<PmsProjectMessage> getProjectMessage(@PathVariable("projectId") String projectId){
-		return pmsProjectMessageFacade.getAllProjectMessage(projectId);
+		return msMessageService.getAllProjectMessage(projectId);
 	}
 
 	/**
@@ -94,7 +100,7 @@ public class MessageController {
 	 */
 	@RequestMapping("/getTaskMsg")
 	public List<PmsProjectMessage> getTaskMessage(@RequestBody final PmsProjectMessage message){
-		return pmsProjectMessageFacade.getProjectTaskMessage(message.getProjectId(), message.getTaskName());
+		return msMessageService.getProjectTaskMessage(message.getProjectId(), message.getTaskName());
 	}
 
 }
