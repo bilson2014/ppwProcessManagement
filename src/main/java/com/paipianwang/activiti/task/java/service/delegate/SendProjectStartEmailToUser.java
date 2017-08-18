72ba8a2c76ddba05ef7ab52f11fd0bc26c9fc5fc
lirui0 <rui000x@foxmail.com>
2017-08-18 17:22:21 +0800

    msg

--- src/main/java/com/paipianwang/activiti/task/java/service/delegate/SendProjectStartEmailToUser.java
+++ src/main/java/com/paipianwang/activiti/task/java/service/delegate/SendProjectStartEmailToUser.java
@@ -8,6 +8,8 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.web.context.ContextLoader;
 
 import com.paipianwang.activiti.mq.email.service.BaseMQService;
+import com.paipianwang.pat.workflow.entity.PmsProjectMessage;
+import com.paipianwang.pat.workflow.facade.PmsProjectMessageFacade;
 
 /**
  * 向客户发送项目启动函
@@ -27,6 +29,16 @@ public class SendProjectStartEmailToUser implements JavaDelegate, Serializable {
 		ApplicationContext context = ContextLoader.getCurrentWebApplicationContext();
 		BaseMQService projectInfoLetterMQService = (BaseMQService) context.getBean("projectInfoLetterMQService");
 		projectInfoLetterMQService.sendMessage(projectId);
+		
+		//留言
+		PmsProjectMessageFacade pmsProjectMessageFacade = (PmsProjectMessageFacade) context.getBean("pmsProjectMessageFacade");
+		PmsProjectMessage message=new PmsProjectMessage();
+		message.setFromId("system");
+		message.setProjectId(projectId);
+				
+				
+		message.setContent("向客户发送项目启动函");
+		pmsProjectMessageFacade.insert(message);
 	}
 
 }
--- src/main/java/com/paipianwang/activiti/task/java/service/delegate/SendSuggestionEmailToTeam.java
+++ src/main/java/com/paipianwang/activiti/task/java/service/delegate/SendSuggestionEmailToTeam.java
@@ -8,6 +8,8 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.web.context.ContextLoader;
 
 import com.paipianwang.activiti.mq.email.service.BaseMQService;
+import com.paipianwang.pat.workflow.entity.PmsProjectMessage;
+import com.paipianwang.pat.workflow.facade.PmsProjectMessageFacade;
 
 /**
  * 向供应商发送修改表
@@ -27,6 +29,15 @@ public class SendSuggestionEmailToTeam implements JavaDelegate, Serializable {
 		ApplicationContext context = ContextLoader.getCurrentWebApplicationContext();
 		BaseMQService projectSampleMideaMQService = (BaseMQService) context.getBean("projectSampleMideaMQService");
 		projectSampleMideaMQService.sendMessage(projectId);
+		//留言
+				PmsProjectMessageFacade pmsProjectMessageFacade = (PmsProjectMessageFacade) context.getBean("pmsProjectMessageFacade");
+				PmsProjectMessage message=new PmsProjectMessage();
+				message.setFromId("system");
+				message.setProjectId(projectId);
+				
+				
+				message.setContent("向供应商发送修改表");
+				pmsProjectMessageFacade.insert(message);
 	}
 
 }
