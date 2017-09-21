1e817715dabe9e084570d3e974fed06c3168e253
lutao <694174250@qq.com>
2017-09-21 19:18:20 +0800

    lt

--- WebContent/resources/js/common.js
+++ WebContent/resources/js/common.js
@@ -8,20 +8,14 @@ var curCounts = 0; // 当前剩余秒数 - 注册
 var InterValObj; // timer变量，控制时间 - 注册
 
 
-<<<<<<< HEAD
 
-//var UrlDo = "www.apaipian.com";
-var UrlDo = "test.apaipian.com";
-//var UrlDo = "localhost";
-//var UrlDo = "192.168.0.157";
 
-=======
 //var UrlDo = "www.apaipian.com";
 //var UrlDo = "test.apaipian.com";
 var UrlDo = "localhost";
-//var UrlDo = "192.168.0.101";
+//var UrlDo = "192.168.0.157";
 
->>>>>>> 9dea43a975bdc3c8a2e72c21b7dd2f21c4c67157
+
 
 
 
